e114dc45b07ea93f1797b1409a537a90acd29487
Hendrix <andriolialessandro93@gmail.com>
2019-02-26 18:52:29 +0100

    added product filtering based on role in product controller. Added validation annotation on base product and base packsize entities

--- src/main/java/com/surveyapp/survey/domain/BasePacksize.java
+++ src/main/java/com/surveyapp/survey/domain/BasePacksize.java
@@ -7,17 +7,24 @@ import lombok.Setter;
 
 import javax.persistence.Column;
 import javax.persistence.MappedSuperclass;
+import javax.validation.constraints.NotBlank;
 
 @MappedSuperclass
 @Getter
 @Setter
 @NoArgsConstructor
+@EqualsAndHashCode
 public abstract class BasePacksize extends BaseEntity{
 
     @Column(name = "Packsize")
+    @NotBlank
     private String packsize;
+
     @Column(name = "Weight")
+    @NotBlank
     private String weight;
+
     @Column(name = "Volume")
+    @EqualsAndHashCode.Exclude
     private boolean volume;
 }
